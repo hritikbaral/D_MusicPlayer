84cf9e2ba30ddf77f53a6f3c61a7167c5a6b78d9
hritikbaral <hritikbaral101@gmail.com>
2026-03-04 16:43:28 +0530

    More refinement for the music player

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const player = document.getElementById("player");
 
 function playSong(song) {
 
-    player.src = song.url;
+    document.getElementById("nowPlayingText").innerText = song.title;
 
-    player.play();
+    player.pause();
+    player.src = "";
+
+    setTimeout(() => {
+
+        player.src = song.url;
+
+        player.play().catch(() => { });
+
+    }, 100);
 
 }
+
+
 
 player.onended = () => {
 
@@ -26,31 +37,112 @@ const playlistUI = document.getElementById("playlist");
 
 let playlist = JSON.parse(localStorage.getItem("playlist")) || [];
 
-const demoSongs = [
-    { title: "Song 1", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" },
-    { title: "Song 2", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" },
-    { title: "Song 3", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3" }
-];
+// const demoSongs = [
+//     { title: "Song 1", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" },
+//     { title: "Song 2", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" },
+//     { title: "Song 3", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3" }
+// ];
 
-function showResults() {
+// function showResults() {
+
+//     results.innerHTML = "";
+
+//     demoSongs.forEach(song => {
+
+//         const div = document.createElement("div");
+//         div.className = "song";
+
+//         div.innerHTML = `
+//  ${song.title}
+//  <button class="addBtn">+</button>
+//  `;
+
+//         div.onclick = () => playSong(song);
+
+//         div.querySelector(".addBtn").onclick = (e) => {
+//             e.stopPropagation();
+//             addToPlaylist(song);
+//         };
+
+//         results.appendChild(div);
+
+//     });
+
+// }
+
+const API_KEY = "AIzaSyA2gfj8NsvgoWyuz57vyPIa6WcvnnO7Jgg";
+
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("keypress", function (e) {
+
+    if (e.key === "Enter") {
+
+        searchYoutube(searchInput.value);
+
+    }
+
+});
+
+
+async function searchYoutube(query) {
+
+    results.innerHTML = "Searching...";
+
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${query}&key=${API_KEY}`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (!data.items) {
+        results.innerHTML = "API Error. Check console.";
+        return;
+    }
+
+    displayResults(data.items);
+
+}
+
+
+
+function displayResults(videos) {
 
     results.innerHTML = "";
 
-    demoSongs.forEach(song => {
+    videos.forEach(video => {
+
+        const videoId = video.id.videoId;
+
+        const title = video.snippet.title;
+
+        const thumbnail = video.snippet.thumbnails.medium.url;
+
+        const song = {
+            title: title,
+            url: `https://YOUR_BACKEND/audio?videoId=${videoId}`
+        };
 
         const div = document.createElement("div");
+
         div.className = "song";
 
         div.innerHTML = `
- ${song.title}
- <button class="addBtn">+</button>
- `;
+        <img src="${thumbnail}" width="120">
+        <span>${title}</span>
+        <button class="addBtn">+</button>
+        `;
 
         div.onclick = () => playSong(song);
 
         div.querySelector(".addBtn").onclick = (e) => {
+
             e.stopPropagation();
+
             addToPlaylist(song);
+
         };
 
         results.appendChild(div);
@@ -58,6 +150,7 @@ function showResults() {
     });
 
 }
+
 
 
 function addToPlaylist(song) {
